66cd2d62f7d041215c186cb89d59edd5717d31b9
antoniosalinasolivares <antonio.salinas1998@comunidad.unam.mx>
2021-01-19 02:45:43 -0600

    removed test lines

--- src/sumaCuadrados.py
+++ src/sumaCuadrados.py
@@ -3,7 +3,3 @@ class SumaCuadrados(Potencia):
     def __init__(self, numero):
         Potencia.__init__(self, numero, 2)
 
-a = SumaCuadrados(123)
-print(a.elementos)
-print(a.exponente)
-print(a.sumaPotencia())
